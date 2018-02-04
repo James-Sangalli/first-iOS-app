a9555561f7db6f316ca95e15676afd188c13ded6
James Sangalli <j.l.sangalli@gmail.com>
2018-02-04 20:01:08 +0800

    went through tutorial to make first basic iOS app: https://www.youtube.com/watch\?v\=5b91dFhZz0g

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,29 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+import Darwin
+
+var str = "Hello, playground"
+var world: String = "hahahah"
+
+let pi = 3.14159
+
+var myAge: Int = 42;
+
+print(Int64.max)
+
+var pi2 : Float = 3.1415
+
+var three: Double = 3
+
+var two: Int = 2
+
+var five = three + Double(two)
+
+var age: Int = 8
+
+if age < 5 {
+    print("lol")
+}
+
+
